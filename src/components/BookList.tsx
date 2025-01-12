775744d676c166eaf43494f163157d361902885f
Amal221200 <amalmurikkoli2000@gmail.com>
2025-01-12 09:54:48 +0530

    Home Page UI complete

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookCard from './BookCard';
 
 interface BookListProps {
   title: string;
@@ -8,8 +9,14 @@ interface BookListProps {
 
 const BookList = ({ title, books, containerClassName }: BookListProps) => {
   return (
-    <section>
-      <h2 className='font-bebas-neue text-4xl text-light-100'>Popular Books</h2>
+    <section className={containerClassName}>
+      <h2 className='font-bebas-neue text-4xl text-light-100'>{title}</h2>
+
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.id} {...book} />
+        ))}
+      </ul>
     </section>
   )
 }
